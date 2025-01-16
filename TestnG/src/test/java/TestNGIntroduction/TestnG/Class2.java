70c75a4b6a8571060ceb8b755d6a3b04271b07d0
nesim <nesim@Nuzrat>
2025-01-16 12:54:15 +0530

    Testng works

---
+++ TestnG/src/test/java/TestNGIntroduction/TestnG/Class2.java
@@ -0,0 +1,12 @@
+package TestNGIntroduction.TestnG;
+
+import org.testng.annotations.Test;
+
+public class Class2 {
+	@Test
+	public void m()
+	{
+		System.out.println("class2...");
+	}
+
+}
---
+++ TestnG/src/test/java/TestNGIntroduction/TestnG/testng.java
@@ -0,0 +1,13 @@
+package TestNGIntroduction.TestnG;
+
+import org.testng.annotations.Test;
+
+public class testng {
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("class1...");
+	}
+	
+}
